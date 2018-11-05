59ee6b6df8076e0335a9919aa4b8ba777a739cc8
Layla Bristol <layla.bristol@gmail.com>
2018-11-05 14:21:08 -0800

    added customer controller test

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -4,4 +4,18 @@ class CustomersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  describe "index" do
+  # These tests are a little verbose - yours do not need to be
+  # this explicit.
+
+    it "successfully gets index page" do
+      get customers_path
+      must_respond_with :success
+    end
+
+  end
+
+
+
 end
