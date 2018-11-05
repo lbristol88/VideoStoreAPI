9084a475a824ae782f29cc293130eab0f18bc7c4
Aurea <fu.sakura@gmail.com>
2018-11-05 12:32:41 -0800

    add tests for customer

--- Gemfile
+++ Gemfile
@@ -38,8 +38,18 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'guard'
+  gem 'guard-minitest'
+
 end
 
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :test do
+  gem 'minitest-rails'
+  gem 'minitest-reporters'
+end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
-class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Customer do
+  let (:customer) {
+    customers(:layla)
+  }
+
+  describe 'relations' do
+    it 'has some rentals' do
+      expect( customer.rentals ).must_respond_to :each
+
+    end
+
+  end
+
+  describe 'validations' do
+
+    it 'is valid when name is present' do
+
+      expect( customer ).must_be :valid?
+
+    end
+
+    it 'is invalid without a name' do
+
+      customer.name = nil
+
+      expect( customer ).wont_be :valid?
+
+    end
+
+  end
 end
