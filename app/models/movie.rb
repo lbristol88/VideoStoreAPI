f363d1e10ef9e038f9154975ed6b5824f7458884
Aurea <fu.sakura@gmail.com>
2018-11-05 11:59:27 -0800

    set model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+
+  has_many :rentals
+
+  validates :title, presence: true
+  validates :inventory, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
 end
