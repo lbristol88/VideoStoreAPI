75567b5872938721b00a8107064a72d246f20026
Layla Bristol <layla.bristol@gmail.com>
2018-11-05 11:35:33 -0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/customer/zomg', to: 'customer#zomg', as: 'zomg'
-  
+  resources :customers, only: [:index]
+  resources :movies, only: [:index, :show, :create]
+
+  get '/rentals/check_in', to: 'rentals#check_in', as: 'check_in'
+  get '/rentals/check_out', to: 'rentals#check_out', as: 'check_in'
+
+  get '/customers/zomg', to: 'customers#zomg', as: 'zomg'
+
 end
