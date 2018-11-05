7149cb4619c4ba17636eb4fdcbb189be2723e19c
Layla Bristol <layla.bristol@gmail.com>
2018-11-05 13:55:01 -0800

    adding model testing

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
-class RentalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Rental do
+  let (:rental) {
+    rentals(:one)
+  }
+
+  describe 'relations' do
+
+    it 'has a customer' do
+      expect( rental.customer ).must_be_instance_of Customer
+
+    end
+
+    it 'has a movie' do
+      expect( rental.movie ).must_be_instance_of Movie
+
+    end
+
+  end
+
+  describe 'validations' do
+
+    it 'is valid when checkout_date is present' do
+      expect( rental ).must_be :valid?
+
+    end
+
+    it 'is invalid when checkout_date is missing' do
+      rental.checkout_date = nil
+      expect( rental ).wont_be :valid?
+
+    end
+
+  end
+
 end
