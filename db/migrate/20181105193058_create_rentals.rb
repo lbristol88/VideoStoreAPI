e55ae93f48b9beaa12348119c84c3dfc1195c710
Aurea <fu.sakura@gmail.com>
2018-11-05 11:43:38 -0800

    add model migrations

--- db/migrate/20181105193058_create_rentals.rb
+++ db/migrate/20181105193058_create_rentals.rb
@@ -2,6 +2,11 @@ class CreateRentals < ActiveRecord::Migration[5.2]
   def change
     create_table :rentals do |t|
 
+      t.date :checkout_date
+      t.date :due_date
+
+      t.belongs_to :movie, index: true
+      t.belongs_to :customer, index: true
       t.timestamps
     end
   end
--- db/migrate/20181105193103_create_movies.rb
+++ db/migrate/20181105193103_create_movies.rb
@@ -1,7 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-
+      t.string :title
+      t.string :overview
+      t.date :release_date
+      t.integer :inventory
       t.timestamps
     end
   end
